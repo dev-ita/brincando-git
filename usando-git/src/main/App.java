fc630e4bcea5bea87ac312b72a788898863d0bb2
Ítalo Oliveira <italo.ods@hotmail.com>
2023-11-17 12:32:05 -0300

    implementada a class GitAuthor

--- usando-git/src/main/App.java
+++ usando-git/src/main/App.java
@@ -4,5 +4,24 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("usando o git");
 		System.out.println("commit 2");
+		
+		GitAuthor gitAuthor = new GitAuthor("dev-ita", "https://github.com/dev-ita/brincando-git.git");
+		
+		System.out.println(gitAuthor.toString());
+	}
+}
+
+class GitAuthor {
+	private String gitAuthorName;
+	private String gitHttps;
+
+	public GitAuthor(String gitAuthorName, String gitHttps) {
+		this.gitAuthorName = gitAuthorName;
+		this.gitHttps = gitHttps;
+	}
+	
+	@Override
+	public String toString() {
+		return this.gitAuthorName + ", " + this.gitHttps + "\n";
 	}
 }
