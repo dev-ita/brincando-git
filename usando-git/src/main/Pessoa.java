8a1769a84f3bfcb582ed591109543aa9b56da400
Ítalo Oliveira <italo.ods@hotmail.com>
2023-11-17 20:45:37 -0300

    Adicionando o atributo idade a classe <Pessoa>

--- usando-git/src/main/Pessoa.java
+++ usando-git/src/main/Pessoa.java
@@ -3,7 +3,14 @@ package main;
 public class Pessoa {
 	private String nome;
 	private String sobrenome;
+	private int idade;
 	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 	public String getNome() {
 		return nome;
 	}
