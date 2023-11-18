ef269eab8a704d8b7e7a3bffb16b37550b89ea6c
Ítalo Oliveira <italo.ods@hotmail.com>
2023-11-18 16:28:24 -0300

    código da taréfa 1, da branch 1

--- usando-git/src/main/GitAuthor.java
+++ usando-git/src/main/GitAuthor.java
@@ -3,10 +3,15 @@ package main;
 class GitAuthor {
 	private String gitAuthorName;
 	private String gitHttps;
+	private String token;
 
 	public GitAuthor(String gitAuthorName, String gitHttps) {
 		this.gitAuthorName = gitAuthorName;
 		this.gitHttps = gitHttps;
+	}
+	
+	public String getToken() {
+		return this.token;		
 	}
 
 	@Override
